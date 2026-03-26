20e2363595c35f6173119cc6c31e2906acc5c80e
repositorykreml1n <realtitus52@gmail.com>
2026-03-26 17:24:11 +0300

    Update kick.lua

--- kick.lua
+++ kick.lua
@@ -5,5 +5,9 @@ if not Mega.Commands then Mega.Commands = {} end
 
 Mega.Commands["/kick"] = function(data)
     local LocalPlayer = game:GetService("Players").LocalPlayer
-    LocalPlayer:Kick("Вы были кикнуты администратором TumbaHub.")
+    
+    -- Проверяем, передали ли нам причину. Если нет — ставим дефолтную.
+    local kickReason = (data and data.reason) and data.reason or "Вы были кикнуты администратором TumbaHub."
+    
+    LocalPlayer:Kick(kickReason)
 end
